fc94a5c83da81eef7d388a35de9734ffc629b7f8
Leonardo Geminiani <leonardogeminiani7@gmail.com>
2026-03-18 15:11:06 +0100

    vite config updated to usage.html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+// vite.config.js
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Replace 'your-repo-name' with your actual repository name
-  base: '/SailChartMaker/', 
+  base: '/SailChartMaker/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        usage: resolve(__dirname, 'usage.html'), // Add secondary files here
+      },
+    },
+  },
 })
